6baf1e4a23d2607b91154debc85b3ecd08070be1
didrikva <didrik.varma@gmail.com>
2025-06-02 17:11:38 +0200

    Added bets and balance if 0, not able to bet more

--- src/Controller/BlackJackControllerStart.php
+++ src/Controller/BlackJackControllerStart.php
@@ -17,14 +17,58 @@ class BlackJackControllerStart extends AbstractController
         return $this->render('blackjack/start.html.twig');
     }
 
-    #[Route('/game/about', name: 'projAbout')]
+    #[Route('/proj/about', name: 'projAbout')]
     public function about(
     ): Response {
         return $this->render('blackjack/about.html.twig');
     }
-    #[Route('/game/start', name: 'projStart')]
+    #[Route('/proj/start', name: 'projStart')]
     public function play(
+        SessionInterface $session,
     ): Response {
-        return $this->render('blackjack/play.html.twig');
+        $amountBet = [0, 0, 0];
+        $chipsLeft = 100;
+        $data = [
+            'placedBet' => false,
+            'betId' => [],
+            'amountBet' => $amountBet,
+            'chipsLeft' => $chipsLeft,
+            'disabled' => true
+        ];
+        $session->set('betId', []);
+        $session->set('placedBet', false);
+        $session->set('amountBet', $amountBet);
+        $session->set('chipsLeft', $chipsLeft);
+        return $this->render('blackjack/play.html.twig', $data);
+    }
+    #[Route('/proj/bet/{id}/{bet}', name: 'projBet')]
+    public function betGET(
+        int $id,
+        int $bet,
+        SessionInterface $session,
+    ): Response {
+        $disabled = true;
+        $betId = $session->get('betId');
+        $placedBet = $session->get('placedBet');
+        $amountBet = $session->get('amountBet');
+        $chipsLeft = $session->get('chipsLeft');
+        $chipsLeft -= $bet;
+        if ($chipsLeft == 0) {
+            $disabled = false;
+        }
+        $amountBet[$id-1] += $bet;
+        $placedBet = True;
+        $betId[] = $id;
+        $data = [
+            'placedBet' => $placedBet,
+            'betId' => $betId,
+            'amountBet' => $amountBet,
+            'chipsLeft' => $chipsLeft,
+            'disabled' => $disabled
+        ];
+        $session->set('betId', $betId);
+        $session->set('amountBet', $amountBet);
+        $session->set('chipsLeft', $chipsLeft);
+        return $this->render('blackjack/play.html.twig', $data);
     }
 }
